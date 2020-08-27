bf0c0acf7ec2f8eec96b4a84345a94eca6465f38
avarpow <648120201@qq.com>
2020-08-27 22:12:38 +0800

    mod path

--- include/Path.hpp
+++ include/Path.hpp
@@ -1,8 +1,9 @@
 #ifndef PATH_HPP
 #define PATH_HPP
 
-class Path {
- public:
+class Path
+{
+public:
   /**
    * user.csv path
    */
@@ -14,9 +15,7 @@ class Path {
   static const char *meetingPath;
 };
 
-// const char *Path::meetingPath = "/tmp/meetings.csv";
-// const char *Path::userPath = "/tmp/users.csv";
-const char *Path::meetingPath = "./t/meetings.csv";
-const char *Path::userPath = "./t/users.csv";
+const char *Path::meetingPath = "/tmp/meetings.csv";
+const char *Path::userPath = "/tmp/users.csv";
 
 #endif
